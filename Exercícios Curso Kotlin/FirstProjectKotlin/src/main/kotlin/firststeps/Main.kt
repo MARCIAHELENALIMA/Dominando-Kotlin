4fdb2f8b442dbd7a6c7df22dc594458999c7b812
Márcia Helena <marciahelenalimac@gmail.com>
2024-03-11 08:16:59 -0300

    atualização do exercicio 1 e a main com metodos e chamada de metodo

--- Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/firststeps/Main.kt
+++ Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/firststeps/Main.kt
@@ -2,9 +2,22 @@ package firststeps
 
 import firststeps.exercicio1.VerificarNumero
 
-fun main(args: Array<String>) {
-    val verificador = VerificarNumero()
-    verificador.verificarPositivoOuNegativo(5)
+class Main {
 
-    println("meu primeiro execicio!")
+    fun testeVerificaNumero() {
+        val verificarNumero = VerificarNumero()
+
+        // Testando com um número positivo
+        verificarNumero.verificarPositivoOuNegativo(5)
+
+        // Testando com um número negativo
+        verificarNumero.verificarPositivoOuNegativo(-3)
+    }
+}
+
+fun main() {
+    val main = Main()
+
+    // Chama o método de teste
+    main.testeVerificaNumero()
 }
--- Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/firststeps/exercicio1/CalculadoraDeIdade.kt
+++ Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/firststeps/exercicio1/CalculadoraDeIdade.kt
@@ -1,4 +1,8 @@
 package firststeps.exercicio1
 
+import java.util.Scanner
 class CalculadoraDeIdade {
+
+
+
 }

9802aaf6cff8d063f5ce755e5b6d37eb69c00dbc
Márcia Helena <marciahelenalimac@gmail.com>
2024-04-06 07:55:15 -0300

    correção dos metodos na main

--- Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/firststeps/Main.kt
+++ Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/firststeps/Main.kt
@@ -1,10 +1,66 @@
 package firststeps
 
+<<<<<<< Updated upstream
 import firststeps.exercicio1.VerificarNumero
+=======
+import firststeps.exerciciosProgressivos.CalculadoraDeMacas
+import firststeps.exerciciosProgressivos.VerificarNumero
+import firststeps.exerciciosProgressivos.CalculadoraDeIdade
+import firststeps.exerciciosProgressivos.VerificarMaiorNumero
+>>>>>>> Stashed changes
 
 fun main(args: Array<String>) {
     val verificador = VerificarNumero()
     verificador.verificarPositivoOuNegativo(5)
 
+<<<<<<< Updated upstream
     println("meu primeiro execicio!")
+=======
+    fun testeVerificaNumero() {
+        val verificarNumero = VerificarNumero()
+        // Testando com um número positivo
+        verificarNumero.verificarPositivoOuNegativo(5)
+        // Testando com um número negativo
+        verificarNumero.verificarPositivoOuNegativo(-3)
+    }
+
+    fun testeCalculadoraDeMacas() {
+        print("Digite a quantidade de maçãs: ")
+        val quantidade = readln().toInt()
+
+        val calculadora = CalculadoraDeMacas(quantidade)
+        val precoFinal = calculadora.getResultado()
+
+        println("Valor total a pagar: \$${precoFinal}")
+    }
+
+    fun testeCalculadoraDeIdade(){
+        print("Qual seu ano de nascimento meu querido? ")
+        val anoNascimentoLido = readln().toInt()
+        val idade = CalculadoraDeIdade.calculaIdade(anoNascimentoLido)//argumento é o valor que passa para o parametro anoNascimento da função
+        print("Você tem $idade seu coroa")
+    }
+
+    fun testeVerificarMaiorNumero(){
+        val verificador = VerificarMaiorNumero()
+        println("Digite três números para comparar qual deles é o maior: ")
+        println("Número 1: ")
+        val numero1 = readln().toInt()
+        println("Número 2: ")
+        val numero2 = readln().toInt()
+        println("Número 3: ")
+        val numero3 = readln().toInt()
+
+        val maiorNumero = verificador.encontrarMaiorNumero(numero1, numero2, numero3)
+        println("O maior número entre os três é: $maiorNumero")
+    }
+}
+
+fun main() {
+    val teste = Main()
+
+    // Chama o método de teste
+    //teste.testeVerificaNumero()
+    teste.testeVerificarMaiorNumero()
+>>>>>>> Stashed changes
 }

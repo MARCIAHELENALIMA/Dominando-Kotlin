eb1b54a615fb3c85f522d379824cb7cfd799f8d9
Márcia Helena <marciahelenalimac@gmail.com>
2024-04-16 08:26:35 -0300

    atualização do main para testar as 2 opções de entrada

--- Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/projetoEstudoComunidadePurple/util/main.kt
+++ Exercícios Curso Kotlin/FirstProjectKotlin/src/main/kotlin/projetoEstudoComunidadePurple/util/main.kt
@@ -1,14 +1,32 @@
 package projetoEstudoComunidadePurple.util
 
+import projetoEstudoComunidadePurple.model.AnaliseAltura
 import projetoEstudoComunidadePurple.model.ConversorTemperatura
 
 fun main() {
 
-    println("Bem-vindo ao programa de conversão de temperaturas!")
+    println("Bem-vindo ao programa de conversão de temperaturas e análise de alturas!")
 
     // Cria instâncias das classes ConversorTemperatura e AnaliseAltura
     val conversorTemperatura = ConversorTemperatura()
+    val analiseAltura = AnaliseAltura()
 
+    // Opções para o usuário
+    println("Escolha uma opção:")
+    println("1. Converter temperatura")
+    println("2. Realizar análise de altura")
+    print("Opção: ")
+
+    when (readlnOrNull()?.toIntOrNull()) {
+        1 -> converterTemperatura(conversorTemperatura)
+        2 -> realizarAnaliseDeAltura(analiseAltura)
+        else -> println("Opção inválida! Por favor, escolha uma opção válida.")
+    }
+
+    println("\nObrigado por usar o programa!")
+}
+
+fun converterTemperatura(conversorTemperatura: ConversorTemperatura) {
     // Recebe entradas do usuário para as temperaturas em Celsius
     print("Digite a temperatura em Celsius para conversão: ")
     val temperaturaCelsius = readlnOrNull()?.toDoubleOrNull()
@@ -30,6 +48,18 @@ fun main() {
     } else {
         println("Temperatura inválida! Por favor, insira um valor numérico válido.")
     }
+}
 
-    println("\nObrigado por usar o programa!")
+fun realizarAnaliseDeAltura(analiseAltura: AnaliseAltura) {
+    // Realizar análise de altura com a lista prévia de informações
+    val numeroMulheres = analiseAltura.contarMulheres()
+    val (maximo, minimo) = analiseAltura.calcularAlturaMaxMin()
+    val mediaAlturaHomens = analiseAltura.calcularMediaAlturaHomens()
+
+    // Exibir os resultados da análise de altura
+    println("\nResultados da análise de altura:")
+    println("Número de mulheres: $numeroMulheres")
+    println("Maior altura: $maximo")
+    println("Menor altura: $minimo")
+    println("Média de altura dos homens: $mediaAlturaHomens")
 }

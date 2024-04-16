b3502396569b687bec41f9021cfebf0039c1aa7d
Márcia Helena <marciahelenalimac@gmail.com>
2024-04-16 08:03:15 -0300

    Adicionada nova classe de teste AnaliseAlturaTest para o modelo AnaliseAltura. A classe inclui três métodos de teste: testContarMulheres para verificar a contagem de mulheres, testCalcularAlturaMaxMin para verificar o cálculo das alturas máxima e mínima, e testCalcularMediaAlturaHomens para validar o cálculo da altura média dos homens. Todos os testes usam assertEquals do JUnit para validação

---
+++ Exercícios Curso Kotlin/FirstProjectKotlin/src/test/kotlin/AnaliseAlturaTest.kt
@@ -0,0 +1,29 @@
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import projetoEstudoComunidadePurple.model.AnaliseAltura
+
+class AnaliseAlturaTest {
+
+    @Test
+    fun testContarMulheres() {
+        val analiseAltura = AnaliseAltura()
+        val numeroMulheres = analiseAltura.contarMulheres()
+        assertEquals(5, numeroMulheres)
+    }
+
+    @Test
+    fun testCalcularAlturaMaxMin() {
+        val analiseAltura = AnaliseAltura()
+        val (maximo, minimo) = analiseAltura.calcularAlturaMaxMin()
+        assertEquals(1.85, maximo, 0.01)
+        assertEquals(1.55, minimo, 0.01)
+    }
+
+    @Test
+    fun testCalcularMediaAlturaHomens() {
+        val analiseAltura = AnaliseAltura()
+        val mediaAlturaHomens = analiseAltura.calcularMediaAlturaHomens()
+        assertEquals(1.7625, mediaAlturaHomens, 0.01)
+    }
+}
